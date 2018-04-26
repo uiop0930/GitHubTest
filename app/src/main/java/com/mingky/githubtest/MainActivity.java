32b3d50fa280a78536495990047d3ed6d8714c96
Mingky <uiop0930@naver.com>
2018-04-26 14:34:55 +0900

    First Commit

--- app/src/main/java/com/mingky/githubtest/MainActivity.java
+++ app/src/main/java/com/mingky/githubtest/MainActivity.java
@@ -2,12 +2,20 @@ package com.mingky.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv= findViewById(R.id.tv);
+        tv.setText("Hello world!");
+        
+
     }
 }
